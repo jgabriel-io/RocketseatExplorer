985fd0bfee3f62b932f0c3555339c09f5cc7aaf2
João Gabriel <s.joaogabriel522@gmail.com>
2024-08-02 11:47:14 -0300

    Atualização FocusTimer

--- Stage05/FocusTimer/src/FocusTimer/timer.js
+++ Stage05/FocusTimer/src/FocusTimer/timer.js
@@ -1,8 +1,12 @@
 import state from './state.js'
 import * as el from './elements.js'
 import { reset } from './actions.js'
+import { kitchenTimer } from './sounds.js'
 
 export function countdown() {
+
+    clearTimeout(state.countdowId)
+
     if(!state.isRunning) {
         return
     }
@@ -17,21 +21,26 @@ export function countdown() {
         minutes--
     }
 
-    if(minutes < 0) {
+    if (minutes < 0) {
         reset()
+        kitchenTimer.play()
         return
     }
 
     updateDisplay(minutes, seconds)
 
-    setTimeout(() => countdown(), 1000)
+
+    state.countdowId = setTimeout(() => countdown(), 1000)
+
 }
+
+
 
 export function updateDisplay(minutes, seconds) {
     minutes = minutes ?? state.minutes
     seconds = seconds ?? state.seconds
 
-
     el.minutes.textContent = String(minutes).padStart(2, "0")
     el.seconds.textContent = String(seconds).padStart(2, "0")
+
 }
--- Stage05/FocusTimer/src/toggle-mode.js
+++ Stage05/FocusTimer/src/toggle-mode.js
@@ -2,12 +2,8 @@ let darkMode = true
 const buttonToggle = document.getElementById('toggle-mode')
 
 buttonToggle.addEventListener('click', (event) => {
-    document.documentElement.classList.toggle('light ')
-
+    document.documentElement.classList.toggle('light')
     const mode = darkMode ? 'light' : 'dark'
-
-    event.currentTarget
-        .querySelector('span').textContent = `${mode} mode ativado!`
-
+    event.currentTarget.querySelector('span').textContent = `${mode} mode ativado!`
     darkMode = !darkMode
 })
