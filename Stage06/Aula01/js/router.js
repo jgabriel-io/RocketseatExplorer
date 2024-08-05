772d48325dd821ac82c2eea810a86ebc9c4b7fa1
João Gabriel <s.joaogabriel522@gmail.com>
2024-08-05 14:00:10 -0300

    Atualização Router - POO

---
+++ Stage06/Aula01/js/router.js
@@ -0,0 +1,29 @@
+class Router {
+    routes = {}
+
+    add(routeName, page) {
+        this.routes[routeName] = page
+    }
+
+    route(event) {
+        event = event || window.event
+        event.preventDefault()
+    
+        window.history.pushState({}, "", event.target.href)
+    
+        this.handle()
+    }
+
+    handle() {
+        const {pathname, href, host, port} = window.location
+        const route = this.routes[pathname] || this.routes[404]
+        fetch(route)
+        .then((data) => data.text())
+        .then((data) => {
+            document.querySelector('#app').innerHTML = html
+        })
+        
+    }
+}
+
+export default new Router()
